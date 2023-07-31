5a81c4dc825a8e7d140641630228890e4e5a635c
Kevin Mary <kmary@chu-tivoli.be>
2023-07-31 07:46:50 +0200

    03 Stop l'observable avec ngOnDestroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,57 +7,7 @@ import { Observable, interval, map, filter, tap, take, concatMap, mergeMap, dela
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  // interval$!: Observable<string>;
-  redTrainsCalled = 0;
-  yellowTrainsCalled = 0;
 
   ngOnInit() {
-    // const interval$ = interval(1000);
-
-    // interval$.subscribe(value => console.log(value));
-
-    // setTimeout(() => {
-    //   interval$.subscribe(value => console.log(value));
-    // }, 3000);
-
-    // Observable simple
-    // this.interval$ = interval(1000);
-
-    // Opérateur de bas niveau
-    // this.interval$ = interval(500).pipe(
-    //   filter(value => value % 3 === 0),
-    //   map(value => value % 2 === 0 ?
-    //     `Le chiffre ${value} est pair.` :
-    //     `Le chiffre ${value} est impair.`),
-    //   tap(text => this.logger(text))
-    // );
-
-    // Opérateur de haut niveau
-    interval(500).pipe(
-      take(10),
-      map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
-      tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
-      mergeMap(color => this.getTrainObservable$(color)), /* mergeMap, concatMap, exhaustMap, switchMap */
-      tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
-    ).subscribe();
-
-  }
-
-  logger(text: string): void {
-    console.log(text);
-  }
-
-  getTrainObservable$(color: 'rouge' | 'jaune') {
-    const isRedTrain = color === 'rouge';
-    isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
-    const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
-    console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
-    return of({ color, trainIndex }).pipe(
-      delay(isRedTrain ? 5000 : 6000)
-    );
-  }
-
-  translateColor(color: 'rouge' | 'jaune') {
-    return color === 'rouge' ? 'red' : 'yellow';
   }
 }
--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,18 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from '../services/face-snaps.service';
+import { tap, interval, take, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-face-snap-list',
   templateUrl: './face-snap-list.component.html',
   styleUrls: ['./face-snap-list.component.scss']
 })
-export class FaceSnapListComponent implements OnInit {
+export class FaceSnapListComponent implements OnInit, OnDestroy {
   faceSnaps!: FaceSnap[];
+  private destroy$!: Subject<boolean>;
 
   constructor(private faceSnapsService: FaceSnapsService) { }
 
   ngOnInit(): void {
     this.faceSnaps = this.faceSnapsService.getAllFaceSnaps();
+
+    // Ici sera passé un bool qui permettra l'arrêt de l'observable
+    this.destroy$ = new Subject<boolean>();
+
+    // Limiter l'Observable à une ou plusieurs valeurs avec l'opérateur take :
+    interval(1000).pipe(
+      // take(5),
+      takeUntil(this.destroy$),
+      tap(value => console.log(value))
+    ).subscribe();
+  }
+
+  // Lorsque j'ai besoin de toutes l'émission de l'Observable et donc le garder en vie, mais sans connaitre à l'avance le nombre d'occurence.
+  // Je vais alors détruire l'observable avec le hook si dessous, il est à l'inverse de ngOntInit car s'execute lorsque le component est détruit. 
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
   }
 }
